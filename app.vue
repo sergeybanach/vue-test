d7729b81092d7b796e127858c867a29548ecec97
sergey banach <sergeybanach@gmail.com>
2023-07-26 01:41:31 +0300

    RedBox uses composables

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <RedBox />
+  </div>
+</template>
+
+<script setup>
+// import { useMouse } from '/mouse.js'
+// const { data, refresh, pending } = await useAsyncData("/api/hello", () =>
+//   $fetch("/api/hello")
+  
+
+// );
+const count = ref(1);
+const double = computed(() => count.value * 2)
+const {x, y} = useMouse();
+
+</script>
---
+++ components/RedBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box">
+    redbox: {{ x }} {{ y }}
+  </div>
+</template>
+
+<script setup>
+const { x, y } = useMouse();
+</script>
+
+<style scoped>
+  .box {
+    width: 500px;
+    height: 200px;
+    background-color: rgb(255, 184, 184);
+  }
+</style>
